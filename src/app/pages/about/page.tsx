72118946872d58127170894d7c77f78acef3d21f
Tharaka-Maheshwara <159180873+Tharaka-Maheshwara@users.noreply.github.com>
2026-05-01 13:13:42 +0530

    About page header okey

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,7 +1,13 @@
+import PrecisionCoolingSection from "@/components/layout/PrecisionCoolingSection";
+
 export default function AboutPage() {
   return (
-    <main className="flex flex-1 items-center justify-center p-8">
-      <h1 className="text-3xl font-semibold">About Us</h1>
+    <main className="flex flex-1 flex-col w-full">
+      <PrecisionCoolingSection />
+      
+      <div className="py-20 px-8 flex items-center justify-center">
+        <h1 className="text-3xl font-semibold">About Us</h1>
+      </div>
     </main>
   );
 }
